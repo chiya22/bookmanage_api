9b6048736d475e2fa27bc82f1dbdee2db4ad5e0a
chiya22 <tomoyay@gmail.com>
2025-08-10 11:59:45 +0900

    必須チェックを追加

--- routers/auth.js
+++ routers/auth.js
@@ -7,7 +7,12 @@ const prisma = new PrismaClient();
 
 //新規ユーザー登録API
 router.post("/register", async (req, res) => {
+
   const { username, email, password } = req.body;
+
+  if (!username || !email || !password ) {
+    return res.status(401).json({ error: '必須項目に値が設定されていません' });
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -25,6 +30,10 @@ router.post("/register", async (req, res) => {
 //ユーザーログインAPI
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password ) {
+    return res.status(401).json({ error: '必須項目に値が設定されていません' });
+  }
 
   const user = await prisma.user.findUnique({ where: { email } });
 
